0bcc6b4a02fde0377bf505e959925dc297d6e564
Sergei <se-panfilov@ya.ru>
2017-07-25 14:33:05 +0800

    try to integrate flow

--- src/storage/reducers/CruiserReducers.js
+++ src/storage/reducers/CruiserReducers.js
@@ -4,7 +4,7 @@ import StorageItemsConstants from '../constants/StorageItemsConstants'
 import Cruiser from '../../constructors/Cruiser.js'
 
 const ACTIONS = {
-	[ActionsConstants.CREATE_CRUISER] (state, action) {
+	[ActionsConstants.CREATE_CRUISER] (state, action): Immutable.Map {
 		const data = new Cruiser(action.data)
 		return state.set(StorageItemsConstants.CRUISER, data)
 	}
@@ -14,13 +14,13 @@ const MESSAGE = {
 	UNKNOWN_ACTION_TYPE: 'UNKNOWN_ACTION_TYPE'
 }
 
-function processAction (state = new Immutable.Map(), action) {
+function processAction (state = new Immutable.Map(), action):  Immutable.Map {
 	// if (!ACTIONS || !action.type || !ACTIONS[action.type]) throw new Error(MESSAGE.UNKNOWN_ACTION_TYPE)
 	if (!ACTIONS || !action.type || !ACTIONS[action.type]) return state
 	return ACTIONS[action.type](state, action)
 }
 
-export default Map = (state = new Immutable.Map(), action) => {
+export default Map = (state = new Immutable.Map(), action): Immutable.Map => {
 	if (!action || !action.type) return state
 	return processAction(state, action)
 }
